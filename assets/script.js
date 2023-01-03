f262e2c0716eff8befbeac5a04fe38fc53398d25
Roksolana Odynak <roksolanaodynak1@gmail.com>
2023-01-03 14:51:08 -0800

    added javascript

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var timerEl = document.getElementById("timer");
+var statEl = document.getElementById("stats");
+var wordDisp = document.getElementById("word-display");
+var startButt = document.getElementById("start-butt");
